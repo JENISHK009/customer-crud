cf7e0545b42f6f3fec94272b1e0995462ad02725
Jenish <kavathiyajenish008@gmail.com>
2024-05-24 23:03:50 +0530

    optimize sorting method

--- src/controller/customerController.js
+++ src/controller/customerController.js
@@ -68,51 +68,16 @@ const getAllCustomers = async ({ query }, res) => {
             }
             : {};
 
-        // sort using method 1 - using mongoose sort method  - Fastest
         const sortCriteria =
             sort === "asc" ? { mobileNumber: 1 } : { mobileNumber: -1 };
-        // const startTime1 = performance.now();
-        const data = await CustomerModel.findOne(filter).sort(sortCriteria);
-        // const endTime1 = performance.now();
-        // const timeTaken1 = endTime1 - startTime1;
+        const result = await CustomerModel.aggregate([
+            { $match: filter },
+            { $sort: sortCriteria },
+            { $limit: 1 }
+        ]);
+        const customer = result[0] || null;
 
-        // // sort using method 2 - using slice and sort
-        // const startTime2 = performance.now();
-        // const method2 = customersWithoutSort.slice().sort((a, b) => sort === 'desc' ? b.mobileNumber.localeCompare(a.mobileNumber) : a.mobileNumber.localeCompare(b.mobileNumber));
-        // const endTime2 = performance.now();
-        // const timeTaken2 = (endTime2 - startTime2) + databaseTime;
-
-        // const startTime3 = performance.now();
-        // // sort using method 3 - using 2 for loops
-        // const method3 = sortingMethod3(customersWithoutSort, sort);
-        // const endTime3 = performance.now();
-        // const timeTaken3 = (endTime3 - startTime3) + databaseTime;
-
-        // const startTime4 = performance.now();
-        // // sort using method 4 - using for loops and if
-        // const method4 = sortingMethod4(customersWithoutSort, sort);
-        // const endTime4 = performance.now();
-        // const timeTaken4 = (endTime4 - startTime4) + databaseTime;
-
-        // const startTime5 = performance.now();
-        // // sort using method 5 - using map
-        // const method5 = sortingMethod5(customersWithoutSort, sort);
-        // const endTime5 = performance.now();
-        // const timeTaken5 = (endTime5 - startTime5) + databaseTime;
-
-        // const startTime6 = performance.now();
-        // // sort using method 6 - using lodash npm
-        // const method6 = sort === 'desc' ? _.sortBy(customersWithoutSort, 'mobileNumber').reverse() : _.sortBy(customersWithoutSort, 'mobileNumber');
-        // const endTime6 = performance.now();
-        // const timeTaken6 = (endTime6 - startTime6) + databaseTime;
-
-        // const startTime7 = performance.now();
-        // // sort using method 7 - using underscore npm
-        // const method7 = sort === 'desc' ? __.sortBy(customersWithoutSort, 'mobileNumber').reverse() : __.sortBy(customersWithoutSort, 'mobileNumber');
-        // const endTime7 = performance.now();
-        // const timeTaken7 = (endTime7 - startTime7) + databaseTime;
-
-        res.status(200).send({ success: true, data });
+        res.status(200).send({ success: true, data: customer });
     } catch (error) {
         handleError(res, error);
     }
